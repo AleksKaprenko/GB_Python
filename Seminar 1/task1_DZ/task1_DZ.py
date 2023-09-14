f235732159acbf12eddef36461f20721b59994d8
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-09-14 18:56:50 +0300

    count unic words in string

--- Seminar 1/task1_DZ/task1_DZ.py
+++ Seminar 1/task1_DZ/task1_DZ.py
@@ -21,8 +21,15 @@
 # print(res)
 
 n=999
-res = 0
+n1 = str(n)
+n1.split()
+res = res1 = 0
 while n>0:
     res += n%10
     n//=10
 print(res)
+
+for i in range(len(n1)):
+    print(n1[i])
+    res1+=int(n1[i])
+print(res1)
--- Seminar 2/task17_DZ/task17_DZ.py
+++ Seminar 2/task17_DZ/task17_DZ.py
@@ -23,28 +23,47 @@
 # Пример:
 # k = 'ноiутбук'
 # 12
-import re
+# import re
 
-dictionary_eng = {1:"A E I O U L N S T R", 
-            2: "D G", 3:"B C M P", 4:"F H V W Y", 5:"K",8:"J X",10:"Q Z"}
-dictionary_ru = {1:"А, В, Е, И, Н, О, Р, С, Т а е", 
-            2: "Д, К, Л, М, П, У", 3:"Б, Г, Ё, Ь, Я", 4:"Й, Ы", 5:", З, Х, Ц, Ч",8:"Ш, Э, Ю",10:"Ф, Щ, Ъ"}
+# dictionary_eng = {1:"A E I O U L N S T R", 
+#             2: "D G", 3:"B C M P", 4:"F H V W Y", 5:"K",8:"J X",10:"Q Z"}
+# dictionary_ru = {1:"А, В, Е, И, Н, О, Р, С, Т а е", 
+#             2: "Д, К, Л, М, П, У", 3:"Б, Г, Ё, Ь, Я", 4:"Й, Ы", 5:", З, Х, Ц, Ч",8:"Ш, Э, Ю",10:"Ф, Щ, Ъ"}
 
+# k='ноутбук'
+# k=k.upper()
+# sum = 0
+# for i in k:
+#     if ord(i) <123: # код последней буквы англ. алфавита Unicode z = 122
+#         for t in dictionary_eng:
+#             sp = dictionary_eng[t]
+#             sp = re.split(" |;|,|\n", sp)
+#             if i in sp:
+#                 sum += t
+#     elif 1040 <= ord(i) <= 1105: # для букв русск алфавита Unicode
+#         for n in dictionary_ru:
+#             sp1 = dictionary_ru[n]
+#             sp1 = re.split(" |;|,|\n", sp1)
+#             if i in sp1:
+#                 sum += n
+# print(sum)
+
+# Вариант 2
+
+list_letters = {1:"AEIOULNSTRАВЕИНОРСТ",
+                2:"DGДКЛМПУ",
+                3:"BCMPБГЁЬЯ",
+                4:"FHVWYЙЫ",
+                5:"KЖЗХЦЧ",
+                8:"JXШЭЮ",
+                10:"QZФЩЪ"}
+
+# word = input("Введите слово: ").upper()
 k='ноутбук'
 k=k.upper()
 sum = 0
 for i in k:
-    if ord(i) <123: # код последней буквы англ. алфавита Unicode z = 122
-        for t in dictionary_eng:
-            sp = dictionary_eng[t]
-            sp = re.split(" |;|,|\n", sp)
-            if i in sp:
-                sum += t
-    elif 1040 <= ord(i) <= 1105: # для букв русск алфавита Unicode
-        for n in dictionary_ru:
-            sp1 = dictionary_ru[n]
-            sp1 = re.split(" |;|,|\n", sp1)
-            if i in sp1:
-                sum += n
+    for key, values in list_letters.items():
+        if i in values:
+            sum += key
 print(sum)
-
