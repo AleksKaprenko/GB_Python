6819e5b5d6205cc30eb1f8d7125b3f4e2b372221
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-09-15 19:53:34 +0300

    add var1 with function

--- Seminar 1/task1_DZ/task1_DZ.py
+++ Seminar 1/task1_DZ/task1_DZ.py
@@ -6,30 +6,33 @@
 # n = 123 -> res = 6 (1 + 2 + 3)
 # n = 100 -> res = 1 (1 + 0 + 0)
 
-# def SumDigits(n, res1):
-#     # res = 0
-#     if n == 0:
-#         return
-#     else: 
-#         res1 += n%10
-#         n//10
-#         SumDigits(n, res1)
+# Var 1
+def SumDigits(n, summ):
+    if n == 0:
+        return summ
+    else: 
+        summ += n%10
+        return SumDigits(n//10, summ)
+
+num =123123123
+res = SumDigits(num, 0)
+print(res)
 
 
-# n =11203351
-# res = SumDigits(n)
-# print(res)
-
+# Var 2
 n=999
-n1 = str(n)
-n1.split()
-res = res1 = 0
+res = 0
 while n>0:
     res += n%10
     n//=10
 print(res)
 
+# Var 3
+
+n=999
+n1 = str(n)
+n1.split()
+res = 0
 for i in range(len(n1)):
-    print(n1[i])
-    res1+=int(n1[i])
-print(res1)
+    res += int(n1[i])
+print(res)
